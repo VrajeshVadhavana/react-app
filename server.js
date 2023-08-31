ad0384787ddea5226f073d4c188ca62c7638b53f
Vadhavana Vrajesh Nileshkumar <119277811+VrajeshVadhavana@users.noreply.github.com>
2023-08-31 16:28:43 +0530

    complete

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 9090;
+var mongodb = require("mongodb").MongoClient;
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname+"/public"))
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/public/index.html");
+});
+app.post("/post",(req,res)=>{
+    console.log(req.body)
+    res.sendFile(__dirname+"/public/index.html");
+    mongodb.connect("mongodb://127.0.0.1:27017/").then((dbs)=>{
+        console.log("database conneted successfully");      
+        var zz = dbs.db("new_d");  
+        zz.collection("react").insertOne({Fullname:req.body.fname}).then(()=>{
+            console.log("Data inserted successfully");
+        }).catch(function(err){
+            console.log("Error");
+        })
+    }).catch(function(err){
+        console.log("Error")
+    })
+})
+app.listen(port,(err)=>{
+    if(err){
+        console.log("Error");
+    }
+    else{
+        console.log("Server is running on "+port);
+    }
+})
